42e6695c1d3dcdf5bcd0f25094f6f5b3925429e4
NirmaniWarakaulla <55423812+NirmaniWarakaulla@users.noreply.github.com>
2021-08-01 19:42:45 +0530

    Create Arrays.cc

---
+++ Data Strctures/Arrays.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int lenth;
+  cin >> lenth;
+  int arr[lenth];
+  for (int i = 0; i < lenth; i++) {
+    cin >> arr[i];
+  }
+  for (int i = lenth-1; i >= 0; i--) {
+    cout << arr[i]<<" ";
+  }
+    return 0;
+}
+
+//try c++ language
