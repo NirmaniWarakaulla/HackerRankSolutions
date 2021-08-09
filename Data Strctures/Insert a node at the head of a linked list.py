bff6d036f53f79328f2b7517e895b2c60094ea35
NirmaniWarakaulla <55423812+NirmaniWarakaulla@users.noreply.github.com>
2021-08-09 23:04:50 +0530

    Create Insert a node at the head of a linked list.py

---
+++ Data Strctures/Insert a node at the head of a linked list.py
@@ -0,0 +1,12 @@
+def insertNodeAtHead(llist, data):
+    # Write your code here
+    x = SinglyLinkedListNode(data)
+    if (llist == None):
+        llist = x
+    else:
+        new_node = x
+        new_node.next = llist
+        llist = new_node
+    return llist
+    
+    // try with python3 language.
